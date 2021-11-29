1f7b3dfcac11999faf45704d98f4712bdc08f35d
Big J <jonathan@moustacherepublic.com>
2021-11-29 17:19:42 +1300

    master: changes on ui and enable upload from front-end

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,23 +1,156 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import axios from "axios";
+import ReactJson from "react-json-view";
+import { FileUploader } from "react-drag-drop-files";
+
+import Icon from "./components/icon";
 
 function App() {
+  const [industry, setIndustry] = useState("");
+  const [transactionCount, setTransactionCount] = useState("");
+  const [transactionVolume, setTransactionVolume] = useState("");
+  const [result, setResult] = useState("");
+  const [totalCost, setTotalCost] = useState("");
+  const [err, setErr] = useState("");
+  const [showUpload, setShowUpload] = useState(true);
+
+  const fileTypes = ["CSV"];
+  const handleChange = (file) => {
+    const config = {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        "x-api-key": process.env.UPLOAD_API_KEY
+      },
+    };
+    const data = new FormData();
+    data.append('file', file);
+    try {
+      axios
+        .post(
+          process.env.UPLOAD_API_ENDPOINT,
+          data,
+          config
+        )
+        .then(function (response) {
+          setResult(response);
+          if (response && response.data) {
+            setTotalCost(response.data.totalCost);
+            setErr("");
+          }
+        })
+        .catch(function (error) {
+          setErr(error.response.data.error);
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const onSubmit = () => {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        "x-api-key": process.env.GETCOST_API_KEY,
+        "Access-Control-Allow-Origin": "*",
+      },
+    };
+    const data = {
+      Industry: industry,
+      TransactionCount: transactionCount,
+      TransactionVolume: transactionVolume,
+    };
+    try {
+      axios
+        .post(
+          process.env.GETCOST_API_ENDPOINT,
+          data,
+          config
+        )
+        .then(function (response) {
+          setResult(response);
+          if (response && response.data) {
+            setTotalCost(response.data.totalCost);
+            setErr("");
+          }
+        })
+        .catch(function (error) {
+          setErr(error.response.data.error);
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const renderJsonViewer = () => {
+    if (result) {
+      return (
+        <div style={{ textAlign: "left" }}>
+          <ReactJson src={result} theme="monokai" />;
+        </div>
+      );
+    }
+    return null;
+  };
+
+  const renderDropZone = () => {
+    if (showUpload) {
+      return (
+        <div>
+          <FileUploader
+            handleChange={handleChange}
+            name="file"
+            types={fileTypes}
+          />
+        </div>
+      );
+    }
+    return null;
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>Merchants Cost Estimator</div>
+        <form>
+          <label>
+            Industry:
+            <input
+              value={industry}
+              onChange={(e) => setIndustry(e.target.value)}
+              type="text"
+              name="industry"
+            />
+          </label>
+          <label>
+            TransactionCount:
+            <input
+              value={transactionCount}
+              onChange={(e) => setTransactionCount(e.target.value)}
+              type="text"
+              name="transactionCount"
+            />
+          </label>
+          <label>
+            TransactionVolume:
+            <input
+              value={transactionVolume}
+              onChange={(e) => setTransactionVolume(e.target.value)}
+              type="text"
+              name="transactionVolume"
+            />
+          </label>
+          <input onClick={onSubmit} type="button" value="Submit" />
+          {totalCost && !err && <div>Total Cost: {totalCost}</div>}
+          {err && <div>{err}</div>}
+        </form>
+        <div onClick={() => setShowUpload(!showUpload)}>
+          <Icon/>
+        </div>
+        {renderDropZone()}
       </header>
+
+      {renderJsonViewer()}
     </div>
   );
 }
